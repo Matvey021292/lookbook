ea5ab3e3e47564cadaa6ff8e16b9cc5952536a15
Matvey021292 <raul021292@gmail.com>
2019-12-26 08:00:21 +0200

    add btn-more

--- resources/views/book/author_content.blade.php
+++ resources/views/book/author_content.blade.php
@@ -1,9 +1,11 @@
 @if($author)
 <section class="AuthorDetailHeader__wrapper row xs-start wp-100">
-    <div class="AuthorDetailHeader__container billetContainerNoOverflow col-md-6">
+    <div class="AuthorDetailHeader__container billetContainerNoOverflow col-md-12">
         <div class="AuthorDetailHeader__image">
             @if($author->desc)
             <img width="128" height="150" src="{{ $author->desc->image }}" alt="{{ $author->title }}" class="AuthorDetailHeader__photo" srcset="{{ $author->desc->image }}">
+            @else
+            <img width="128" height="150" src="{{ asset(env("THEME")) }}/images/190x288.jpg" alt="{{ $author->title }}" class="AuthorDetailHeader__photo" >
             @endif
         </div>
         <div class="billetContainerWrapper p-0 ">
@@ -14,12 +16,15 @@
         </div>
     </div>
 </section>
-<div class="AuthorDetailHeader__container billetContainerNoOverflow AuthorDetailView__containerAbout">
+@if($author->desc && $author->desc->desc_author)
+<div data-toggle="collapse" class="AuthorDetailHeader__container billetContainerNoOverflow AuthorDetailView__containerAbout">
     <h3 class="section-title">Об авторе</h3>
-    @if($author->desc)
-    <p class="des-author">{!! $author->desc->desc_author  !!} </p>   
-    @endif 
+    <div>
+        <p class="des-author">{!! $author->desc->desc_author  !!} </p>  
+    </div>
+    <span class="toggle-icon"><i class="fas fa-angle-down"></i></span> 
 </div>
+@endif 
 @else
 <div class="page-title-author w-100">
     <div class="wrap-content">

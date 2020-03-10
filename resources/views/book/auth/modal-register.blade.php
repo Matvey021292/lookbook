59ad46215e2b4de5b8c65d89d2c2678e248ac764
Matvey021292 <raul021292@gmail.com>
2020-03-10 22:37:33 +0200

    edit modal register

--- resources/views/book/auth/modal-register.blade.php
+++ resources/views/book/auth/modal-register.blade.php
@@ -2,15 +2,18 @@
     <div class="modal-content">
         <span class="close-button">&times;</span>
         <div class="card AuthFormDialog__inner">
-            <div class="card-header title-1">{{ __('Register') }}</div>
+            <div class="card-header title-1">{{ __('Создать аккаунт') }}</div>
             <div class="card-body jest-auth-form">
                 <form class="ajax" method="POST" action="{{ route('registration') }}">
                     @csrf
                     <div class="form-group  Form__formRow">
                         <span class="FormTextInput__formTextInput">
-                            <label for="name" class="FormTextInput__label col-form-label text-md-right">{{ __('Name') }}</label>
+                            <label for="name"
+                                   class="FormTextInput__label col-form-label text-md-right">{{ __('Логин') }}</label>
                             <div class="FormTextInput__control">
-                                <input id="reg-name" type="text" class="FormTextInput__input form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="reg-name" type="text"
+                                       class="form-control @error('name') is-invalid @enderror" name="name"
+                                       value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
                                 <span class="invalid-feedback" role="alert">
@@ -23,11 +26,13 @@
 
                     <div class="form-group  Form__formRow">
                         <span class="FormTextInput__formTextInput">
-                            <label for="email" class="FormTextInput__label col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email"
+                                   class="FormTextInput__label col-form-label text-md-right">{{ __('Адрес электронной почты') }}</label>
 
                             <div class="FormTextInput__control">
-                                <input id="reg-email" type="email" class="FormTextInput__input form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
+                                <input id="reg-email" type="email"
+                                       class="form-control @error('email') is-invalid @enderror" name="email"
+                                       value="{{ old('email') }}" required autocomplete="email">
                                 @error('email')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -38,10 +43,13 @@
                     </div>
                     <div class="form-group  Form__formRow">
                         <span class="FormTextInput__formTextInput">
-                            <label for="password" class="FormTextInput__label col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password"
+                                   class="FormTextInput__label col-form-label text-md-right">{{ __('Пароль') }}</label>
 
                             <div class="FormTextInput__control">
-                                <input id="reg-password" type="password" class="FormTextInput__input form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="reg-password" type="password"
+                                       class="form-control @error('password') is-invalid @enderror" name="password"
+                                       required autocomplete="new-password">
 
                                 @error('password')
                                 <span class="invalid-feedback" role="alert">
@@ -54,18 +62,33 @@
 
                     <div class="form-group  Form__formRow">
                         <span class="FormTextInput__formTextInput">
-                            <label for="password-confirm" class="FormTextInput__label col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            <label for="password-confirm"
+                                   class="FormTextInput__label col-form-label text-md-right">{{ __('Подтвердите пароль') }}</label>
 
                             <div class="FormTextInput__control">
-                                <input id="reg-password-confirm" type="password" class="FormTextInput__input form-control" name="password_confirmation" required autocomplete="new-password">
+                                <input id="reg-password-confirm" type="password" class="form-control"
+                                       name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </span>
                     </div>
                     <div class="form-group  Form__formRow mb-0">
 
-                        <button type="submit" class="Button__primaryButton btn btn-primary">
-                            {{ __('Register') }}
+                        <button type="submit" class="custom_btn btn btn-primary">
+                            {{ __('Регистрация') }}
                         </button>
+                    </div>
+                    <div class="group_btn">
+                        <a href="/register" data-modal="#register"
+                           class="trigger custom_btn custom_btn__outline button-social">
+                            <i class="icon icon-google"></i>
+                        </a>
+                        <a href="/register" data-modal="#register"
+                           class="trigger custom_btn custom_btn__outline button-social">
+                            <i class="icon icon-facebook"></i>
+                        </a>
+                        <a href="/login" data-modal="#auth" class="trigger custom_btn custom_btn__outline">
+                            <span>{{__('Вход')}}</span>
+                        </a>
                     </div>
                     @include(env('THEME') . '.custom.loader')
                 </form>
